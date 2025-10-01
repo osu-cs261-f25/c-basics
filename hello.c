66f5c40b0ca456c4f30b3ae1c8a367c98cc839de
Rob Hess <hessro@oregonstate.edu>
2025-10-01 10:51:34 -0700

    Add code from 10/1 lecture.

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  int n = 16;
+  double pi = 3.1415;
+  float f = 4.5;
+  char c = 'a';
+
+  // n = 4.75;
+
+  printf("Hello, world!\n");
+
+  printf("== n: %d f: %.4lf\n", n, pi);
+}
